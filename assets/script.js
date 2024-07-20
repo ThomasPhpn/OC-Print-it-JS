21919289f466d18a6ac98f45e2a6702786bf3544
ThomasPhpn <tphilipponneau@gmail.com>
2024-07-20 15:25:00 +0200

    Ajout du javascript pour les fleches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,46 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let currentIndex = 0;
+const baseURL = './assets/images/slideshow/';
+
+function showSlide(index) {
+	if (index >= slides.length) {
+		currentIndex = 0;
+	} else if (index < 0) {
+		currentIndex = slides.length - 1;
+	} else {
+		currentIndex = index;
+	}
+
+	const imageElement = document.getElementById('banner-img');
+	const taglineElement = document.getElementById('tagline');
+	imageElement.src = baseURL + slides[currentIndex].image;
+	taglineElement.innerHTML = slides[currentIndex].tagLine;
+
+	updateDots();
+}
+
+function nextSlide() {
+	showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+	showSlide(currentIndex - 1);
+}
+
+function updateDots() {
+	const dots = document.querySelectorAll('.dot');
+	dots.forEach((dot, index) => {
+		dot.classList.remove('dot_selected');
+		if (index === currentIndex) {
+			dot.classList.add('dot_selected');
+		}
+	});
+}
+
+document.getElementById('arrow-right').addEventListener('click', nextSlide);
+document.getElementById('arrow-left').addEventListener('click', prevSlide);
+
+// Initialiser la première diapositive
+showSlide(currentIndex);
